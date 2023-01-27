60cd7ea15f311a7a8ff3b420d3a1bbda3c3d3031
karmy23 <40173788+mamemakimameo@users.noreply.github.com>
2023-01-27 10:01:59 +0900

    全てにスラッシュを付けてみた

--- src/constants/navlink.ts
+++ src/constants/navlink.ts
@@ -1,103 +1,101 @@
-import React from "react"
-
 export default [
     {
         "id": 1,
         "title": "ABOUT",
-        "link": "/about",
+        "link": "/about/",
         "listMenu": [
             {
                 "id": 1.1,
                 "text": "代表挨拶",
-                "link": "/about#greeting",
+                "link": "/about#greeting/",
             },
             {
                 "id": 1.2,
                 "text": "理念",
-                "link": "/about#philosophy",
+                "link": "/about#philosophy/",
             },
             {
                 "id": 1.3,
                 "text": "法人概要",
-                "link": "/about#overview",
+                "link": "/about#overview/",
             },
         ],
     },
     {
         "id": 2,
         "title": "ACTIVITY",
-        "link": "/activity",
+        "link": "/activity/",
         "listMenu": [
             {
                 "id": 2.1,
                 "text": "教育",
-                "link": "/activity/#education",
+                "link": "/activity/#education/",
             },
             {
                 "id": 2.2,
                 "text": "イベント",
-                "link": "/activity/#event",
+                "link": "/activity/#event/",
             },
             {
                 "id": 2.3,
                 "text": "コミュニティ",
-                "link": "/activity/#community",
+                "link": "/activity/#community/",
             },
         ],
     },
     {
         "id": 3,
         "title": "EVENT",
-        "link": "/event",
+        "link": "/event/",
         "listMenu": [
             {
                 "id": 3.1,
                 "text": "全て",
-                "link": "/event",
+                "link": "/event/",
             },
             {
                 "id": 3.2,
                 "text": "開催前イベント",
-                "link": "/event/#before",
+                "link": "/event/#before/",
             },
             {
                 "id": 3.3,
                 "text": "終了イベント",
-                "link": "/event/#end",
+                "link": "/event/#end/",
             },
         ],
     },
     {
         "id": 4,
         "title": "NEWS",
-        "link": "/blog",
+        "link": "/blog/",
         "listMenu": [
             {
                 "id": 4.1,
                 "text": "お知らせ",
-                "link": "/blog/#information",
+                "link": "/blog/#information/",
             },
             {
                 "id": 4.2,
                 "text": "プレスリリース",
-                "link": "/blog/#release",
+                "link": "/blog/#release/",
             },
             {
                 "id": 4.3,
                 "text": "活動報告",
-                "link": "/blog/#report",
+                "link": "/blog/#report/",
             },
         ],
     },
     {
         "id": 5,
         "title": "CONTACT",
-        "link": "/contact",
+        "link": "/contact/",
         "listMenu": [
             {
                 "id": 5.1,
                 "text": "お問い合わせ",
-                "link": "/contact",
+                "link": "/contact/",
             },
         ],
     },
