0421fc26e67c870ade1aa494e8b3f1deed886afd
arch-mania <98379199+arch-mania@users.noreply.github.com>
2023-11-30 00:34:26 +0900

    change ceo.gatsbyjs.ioをjceoa.orgにredirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,22 @@
 exports.createPages = async ({ graphql, actions }) => {
-    const { createRedirect } = actions;
+  const { createRedirect } = actions;
 
-    createRedirect({
-        fromPath: `/blog/post/*/`,
-        toPath: `/blog/post/*`,
-    });
+  createRedirect({
+    fromPath: `/blog/post/*/`,
+    toPath: `/blog/post/*`,
+    isPermanent: true,
+  });
 
-    createRedirect({
-        fromPath: `/event/post/*/`,
-        toPath: `/event/post/*`,
-    });
-}
+  createRedirect({
+    fromPath: `/event/post/*/`,
+    toPath: `/event/post/*`,
+    isPermanent: true,
+  });
+
+  createRedirect({
+    fromPath: 'https://ceo.gatsbyjs.io/',
+    toPath: 'https://jceoa.org/',
+    isPermanent: true,
+    force: true,
+  });
+};
